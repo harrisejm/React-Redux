0e2c78d33a8464d892589714246aaf900c24d66e
Eddie Harris <harrisejm@gmail.com>
2018-09-18 14:04:24 -0700

    add initial files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,73 @@
-const userClick = () => {
-  console.log('click');
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
+
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
 }
+
+
+const reducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case 'NEXT_LYRIC':
+      let newArrayPosition = state.arrayPosition + 1;
+      newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition,
+      }
+      return newState;
+    case 'RESTART_SONG':
+      newState = initialState;
+      return newState;
+    default:
+      return state;
+  }
+}
+
+///
+const { expect } = window;
+
+expect(reducer(initialState, { type: null })).toEqual(initialState);
+
+expect(reducer(initialState, { type: 'NEXT_LYRIC'})).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+});
+
+expect(reducer({
+    songLyricsArray: songLyricsArray,
+    arrayPosition: 1,
+  },
+  { type: 'RESTART_SONG' })
+).toEqual(initialState);
+
+
+////
+
+/// UI
+const { createStore } = Redux;
+const store = createStore(reducer);
+
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
+window.onload = function() {
+  renderLyrics();
+}
+
+const userClick = () => {
+  if (store.getState().arrayPosition === store.getState().songLyricsArray.length -1) {
+    store.dispatch({ type: 'RESTART_SONG'});
+  } else {
+  store.dispatch({ type: 'NEXT_LYRIC'});
+  console.log(store.getState());
+  }
+}
+store.subscribe(renderLyrics);
